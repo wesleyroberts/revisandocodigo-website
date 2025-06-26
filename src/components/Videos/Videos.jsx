4d4e8b0406db66f4a32bd0cf044eb281936a732f
wesleyroberts <wesleyroberts.office@gmail.com>
2025-06-25 21:04:03 -0300

    Corrigi redirecionamentos de vídeos.

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -6,37 +6,43 @@ const Videos = () => {
       title: 'Criando um React Client Extension',
       description: 'Aprenda a criar um modulo Client Extension do tipo frontend usando o framework React com o vite.',
       thumbnail: 'https://i.ytimg.com/vi/CCHHZrqowfw/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLC5azW6rwMQZPhLm4v_CoeJ-BgLcg',
-      duration: '15:30'
+      duration: '10:26',
+      link:'https://www.youtube.com/watch?v=CCHHZrqowfw'
     },
     {
       title: 'Visita Exclusiva: Tecnologia Avançada na Liferay - Controle de Drones com Client Extensions!',
       description: 'Confira o que rolou no meetup presencial em Recife-PE, com direito a visita ao escritório da Liferay.',
       thumbnail: 'https://i.ytimg.com/vi/H_pO-zUkJ2o/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLDMea7ue0n8BAnh0x65epS6DbqAqQ',
-      duration: '22:45'
+      duration: '5:11',
+      link:'https://www.youtube.com/watch?v=H_pO-zUkJ2o'
     },
     {
       title: 'Liferay Academy Brasil - Gerenciamento de Conteúdo Web #03',
       description: 'Conheça as melhores práticas para gestão de conteúdos dentro do liferay.',
       thumbnail: 'https://i.ytimg.com/vi/7GKj9LX6cT0/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLA27qeXx2wS5SbfM6k5PBO89u9mBQ',
-      duration: '18:20'
-    },
-    {
-      title: 'Liferay Academy Brasil - Gerenciamento de Conteúdo Web #03',
-      description: 'Como integrar o Liferay com APIs externas usando REST Services.',
-      thumbnail: 'https://i.ytimg.com/vi/7GKj9LX6cT0/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLA27qeXx2wS5SbfM6k5PBO89u9mBQ',
-      duration: '25:10'
+      duration: '23:43',
+      link:'https://www.youtube.com/watch?v=7GKj9LX6cT0&'
     },
     {
       title: 'Liferay Academy Brasil - Gerenciamento de Sites #02',
       description: 'Aprenda a criar sites e receba dicas para gerenciar-los da melhor forma.',
       thumbnail: 'https://i.ytimg.com/vi/7GKj9LX6cT0/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLA27qeXx2wS5SbfM6k5PBO89u9mBQ',
-      duration: '20:15'
+      duration: '13:51',
+      link:'https://www.youtube.com/watch?v=AG5hjBqqbdY'
     },
     {
       title: 'Liferay Academy Brasil -  Navegação e Funcionalidades Essenciais #01',
       description: 'Guia completo sobre os principais recursos de gestão do portal Liferay.',
       thumbnail: 'https://i.ytimg.com/vi/7GKj9LX6cT0/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLA27qeXx2wS5SbfM6k5PBO89u9mBQ',
-      duration: '30:05'
+      duration: '13:22',
+      link:'https://www.youtube.com/watch?v=JBY8H5--5Ds&t=660s'
+    },
+    {
+      title: 'Automatize seu Conteúdo Web com Liferay e GPT: Transforme sua Estratégia de Marketing',
+      description: 'Neste vídeo, mergulhamos no mundo da gestão de experiência digital e na revolução da inteligência artificial com o objetivo de transformar sua estratégia de conteúdo web.',
+      thumbnail: 'https://i.ytimg.com/vi/7GKj9LX6cT0/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLA27qeXx2wS5SbfM6k5PBO89u9mBQ',
+      duration: '3:45',
+      link:'https://www.youtube.com/watch?v=k8eKJWmpJ_8'
     }
   ];
 
@@ -63,9 +69,7 @@ const Videos = () => {
                 <div className="card-body d-flex flex-column">
                   <h5 className="card-title text-primary">{video.title}</h5>
                   <p className="card-text flex-grow-1">{video.description}</p>
-                  <button className="btn btn-primary mt-auto">
-                    Assistir Agora
-                  </button>
+                  <a class="btn btn-primary mt-auto" href={video.link} role="button">Assistir Agora</a>
                 </div>
               </div>
             </div>
